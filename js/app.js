a8031a216b32a8dc40b11fe4840f8b881388c695
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-05-15 14:46:12 -0400

    logs random pkmn from api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log($);
+$(() => {
+  let number = Math.floor(Math.random() * 809 + 1)
+
+  $.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/" + number,
+    type: "GET",
+  }).then((data) => {
+    console.log(data);
+    //end of then
+  }).catch((error) => {
+    console.log('Sorry, an error has occured!');
+  })//end of err 
+
+  //end of on doc load
+})
